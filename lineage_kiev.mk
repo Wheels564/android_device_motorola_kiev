28814376b986c936fd190ca2ac58d1a0fb4185b9
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2024-03-27 15:18:37 -0400

    enable microg services

--- lineage_kiev.mk
+++ lineage_kiev.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/motorola/kiev/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some common PARTNER_GMS stuff.
+$(call inherit-product, vendor/partner_gms/products/gms.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_kiev
 PRODUCT_DEVICE := kiev
