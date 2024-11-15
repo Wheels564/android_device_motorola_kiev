4f84da70a1b0056d2f2d7c47a895ab8ff6631e1d
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2024-11-15 13:14:44 -0500

    add gms update to crdroid 15

--- lineage_kiev.mk
+++ lineage_kiev.mk
@@ -14,27 +14,27 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some common PARTNER_GMS stuff.
+$(call inherit-product, vendor/partner_gms/products/gms.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_kiev
 PRODUCT_DEVICE := kiev
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g 5G
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G 5G
+PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-
-# Inherit some common PARTNER_GMS stuff.
-$(call inherit-product, vendor/partner_gms/products/gms.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Build info
-BUILD_FINGERPRINT := "motorola/kiev_retail/kiev:11/RZKS31.Q3-45-16-8-19/cc8087:user/release-keys"
+#BUILD_FINGERPRINT := "motorola/kiev_retail/kiev:11/RZKS31.Q3-45-16-8-19/cc8087:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=kiev_retail \
-    PRIVATE_BUILD_DESC="kiev_retail-user 11 RZKS31.Q3-45-16-8-19 cc8087 release-keys"
+    DeviceProduct=kiev_retail \
+    BuildDesc="kiev_retail-user 11 RZKS31.Q3-45-16-8-19 cc8087 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
