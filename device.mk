a5a9dae3c2543b5ac82b6789ac05e66f73bb1ac7
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2026-07-02 14:55:47 -0400

    Update maintainer property in device.mk

--- device.mk
+++ device.mk
@@ -77,7 +77,7 @@ PRODUCT_PACKAGES += \
 
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.maintainer=Wheels564  
+    ro.crdroid.maintainer=Wheels564 
 
 # Media
 PRODUCT_COPY_FILES += \
